f54782ef413e40dd8e660e32ce186ecdcf70ab41
Evgenii <jenek0907@mail.ru>
2020-12-31 18:30:37 +0300

    coming up element on 3-d page

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -5,24 +5,47 @@ export default class Slider {
         this.btns = document.querySelectorAll(btns);
         this.slideIndex = 1;
     }
+
     showSlides(n) {
+        
         if (n > this.slides.length) {
             this.slideIndex = 1;
         }
         if (n < 1) {
             this.slideIndex = this.slides.length;
         }
+        try {
+            this.hanson.style.opacity = '0';
+            if(n == 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch (error) {
+            
+        }
         this.slides.forEach(slide => {
             slide.style.display = 'none';
             
         });
         this.slides[this.slideIndex - 1].style.display = 'block';
+        
 
     }
     plusSlides(n) {
         this.showSlides(this.slideIndex += n)
     }
     render() {
+        try {
+            this.hanson = document.querySelector('.hanson');
+            
+        } catch (error) {
+            
+        }
         this.btns.forEach(btn => {
             btn.addEventListener('click', () => {
                 this.plusSlides(1);
