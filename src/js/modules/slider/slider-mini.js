d4d640558c4d375f71a005818e1948cfcad5bb80
Evgenii <jenek0907@mail.ru>
2021-01-01 22:07:47 +0300

    try-catch

--- src/js/modules/slider/slider-mini.js
+++ src/js/modules/slider/slider-mini.js
@@ -59,7 +59,8 @@ export default class MiniSlider extends Slider {
     }
 
     init() {
-        this.container.style.cssText = `
+        try {
+            this.container.style.cssText = `
             display: flex;
             flex-wrap: wrap;
             overflow: hidden;
@@ -71,6 +72,9 @@ export default class MiniSlider extends Slider {
 
         if (this.autoplay) {
             setInterval(() => this.nextSlide(), 5000);
+        }
+        } catch (error) {
+            
         }
     }
 }
